8932f0556bcc26c29c21e618c4cee1246f331a9a
Ary Setiyawan <arysetiyawan@sindata.net>
2022-06-19 23:29:52 +0800

    First Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/parys410/app-say-hello
+
+go 1.18
+
+require github.com/parys410/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/parys410/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Ary"))
+}
